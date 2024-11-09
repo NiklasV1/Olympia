7a9d1d1d563dc8c18de8df35f9dfd03e43194a96
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2024-11-09 14:07:37 +0100

    OlympicEntity created + Test Structure

--- src/main/java/OlympicEntity.java
+++ src/main/java/OlympicEntity.java
@@ -1,2 +1,47 @@
-public class OlympicEntity {
+import javax.swing.*;
+import java.util.Objects;
+
+public class OlympicEntity implements Comparable<OlympicEntity> {
+    private final int id;
+    private final String name;
+
+    public OlympicEntity(int id, String name) {
+        if (name == null) {
+            throw new IllegalArgumentException();
+        }
+
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj.getClass() == this.getClass()) {
+            OlympicEntity entity = (OlympicEntity) obj;
+            return entity.getId() == id;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public int compareTo(OlympicEntity o) {
+        return switch (Integer.compare(id, o.getId())) {
+            case -1 -> -1;
+            case 1 -> 1;
+            default -> 0;
+        };
+    }
 }
---
+++ src/test/java/OlympicEntityTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class OlympicEntityTest {
+
+    @Test
+    void testContructor() {
+
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void compareTo() {
+    }
+}
